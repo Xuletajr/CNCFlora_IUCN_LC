f0ea57a89ee5e168606d3f1ee5f7f3dd32faacc0
Jose Wagner Ribeiro Junior <jwrj@rc.unesp.br>
2019-09-19 10:28:22 -0300

    Formatação
    Adicionando informações ao script para auxiliar no acompanhamento do código.

--- scripts/0 filtrar inpa.R
+++ scripts/0 filtrar inpa.R
@@ -1,8 +1,15 @@
-#filtrar o arquivo do inpa
+# filtrar o arquivo do inpa
+
+# Ler os pacotes
 library(readxl)
+library(dplyr)
+
+# Ler a planilha de dados proveniente do speciesLink
 tabela_inpa_splink <- read_excel(path = "./data/planilha_geral_splink_inpa.xlsx", sheet = 1)
 
-library(dplyr)
+# Ler a planilha de espécies do recorte após ter sido gerada pelo script "02 flora"
 head(unique(tabela_inpa_splink$scientificname))
 inpa_filtered <- tabela_inpa_splink %>% filter(tabela_inpa_splink$scientificname %in% especies)
+
+# Exportando uma planilha com os dados do speciesLink filtrados pelas espécies de interesse
 write.csv(inpa_filtered, "./output/inpa_filtrado.csv")
